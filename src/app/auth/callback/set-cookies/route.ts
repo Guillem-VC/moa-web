50311fec113ee642a4992de0bc27df94336cd5e9
Guillem <guillem.vazcas@gmail.com>
2025-12-10 12:42:23 +0100

    10/12 v1

--- src/app/auth/callback/set-cookies/route.ts
+++ src/app/auth/callback/set-cookies/route.ts
@@ -2,8 +2,16 @@ import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   const { access_token, refresh_token } = await req.json()
+  
+  console.log("HEADERS:")
+  req.headers.forEach((v, k) => console.log(`  ${k}: ${v}`))
+
+  
+  console.log("  access_token:", access_token)
+  console.log("  refresh_token:", refresh_token)
 
   const res = NextResponse.json({ ok: true })
+
 
   res.cookies.set('sb-access-token', access_token, {
     httpOnly: true,
